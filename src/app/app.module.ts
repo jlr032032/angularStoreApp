00c1e75121241ff780836b1156d8c93482313922
Jorge <jlr032032@gmail.com>
2019-09-08 00:28:39 -0400

    Establece estado inicial de aplicación tutorial de Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule
+  ],
+  declarations: [
+    AppComponent,
+    TopBarComponent,
+    ProductListComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
