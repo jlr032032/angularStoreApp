567aa40ada9d8744c719540a8409fdade433ed3a
Jorge <jlr032032@gmail.com>
2019-09-09 20:14:15 -0400

    Crea componente destinado a la vista de detalles de producto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 @NgModule({
 	imports: [
@@ -14,7 +15,8 @@ import { ProductAlertsComponent } from './product-alerts/product-alerts.componen
 		AppComponent,
 		TopBarComponent,
 		ProductListComponent,
-		ProductAlertsComponent
+		ProductAlertsComponent,
+		ProductDetailsComponent
 	],
 	providers: [],
 	bootstrap: [AppComponent]
